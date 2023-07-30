b8993f9aa29956ff29737c90298f445b4fd49f85
sailbirds <sailbirds@luminesence.jp>
2023-07-30 20:02:31 +0900

    fix: category page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -4,10 +4,11 @@ import getProducts from '@/actions/get-products';
 import getSizes from '@/actions/get-sizes';
 import Billboard from '@/components/billboard';
 import Container from '@/components/ui/container';
-import Filter from './comonents/filter';
 import NoResults from '@/components/ui/no-results';
 import ProductCard from '@/components/ui/product-card';
-import MobileFilters from './comonents/mobile-filters';
+
+import MobileFilters from './components/mobile-filters';
+import Filter from './components/filter';
 
 export const revalidate = 0;
 
@@ -39,7 +40,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     <div className="bg-white">
       <Container>
         <Billboard data={category.billboard} />
-        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+        <div className="px-4 pb-24 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
@@ -48,7 +49,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
                 {products.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
